f71ff5542bb69cf62ea61f5810882b21f64d2bff
LoveLess <116763405+LoveLess1212@users.noreply.github.com>
2026-06-29 20:56:57 +0700

    something dum[

--- src/bpmn_layout_server/server.js
+++ src/bpmn_layout_server/server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+  next(); asdfasdfasdf
 });
 
 app.get('/', (req, res) => {
